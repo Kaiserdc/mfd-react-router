0f07de1766b00b3e1f48e026578b1c71fa202d04
kaiserdc <koreshdc@gmail.com>
2025-04-29 15:04:11 +0300

    feat: Предварительный комит

--- src/pages/Character/Сharacter.tsx
+++ src/pages/Character/Сharacter.tsx
@@ -1,22 +1,38 @@
-// import {getCharacter} from "../lib/utilities";
 import {Navigate, useParams} from "react-router-dom";
+import {getCharacterById} from "../../lib/dataService.ts";
+import {Character} from "../../interfaces";
 
 export function Character() {
-    // const {id} = useParams();
-    // const character = getCharacter(id)
+    const {id} = useParams<{ id: string }>();
+    const character: Character | undefined = id ? getCharacterById(id) : undefined;
     //
     // if (!character) {
     //     return <Navigate to={'*'}/>
     // }
-
+    if (!character) {
+        return <Navigate to="*" replace/>
+    }
     return <>
-        <h1>Детальная информация</h1>
-        {/*<img className={'mb-4'} src={character.image} alt=''/>*/}
-        <ul className="list-unstyled">
-            {/*{Object.keys(character).map(key => {*/}
-            {/*    if (key === 'image' || key === 'name' ) return*/}
-            {/*    return <li>{key}: {character[key]}</li>*/}
-            {/*})}*/}
-        </ul>
+        <div className="container my-4">
+            <div className="row">
+                <div className="col-md-4">
+                    <img
+                        src={character.image}
+                        alt={character.name}
+                        className="img-fluid rounded"
+                        loading="lazy"
+                    />
+                </div>
+                <div className="col-md-8">
+                    <h1>{character.name}</h1>
+                    <p><strong>Status:</strong> {character.status}</p>
+                    <p><strong>Species:</strong> {character.species}</p>
+                    <p><strong>Gender:</strong> {character.gender}</p>
+                    {character.origin && <p><strong>Origin:</strong> {character.origin.name}</p>}
+                    {character.location && <p><strong>Location:</strong> {character.location.name}</p>}
+                    {/* при желании можно добавить больше полей */}
+                </div>
+            </div>
+        </div>
     </>
 }
