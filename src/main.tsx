0573f5b5054cfc5d7cb0dfceeed5afdff22751de
kaiserdc <koreshdc@gmail.com>
2025-05-06 14:15:22 +0300

    feat: Добавил локальную проверку ошибок ErrorBoundry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {createRoot} from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router-dom";
 import {AuthProvider} from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
